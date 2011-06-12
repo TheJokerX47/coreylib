dc88df523ab4496ca6459e632b0be10a2ca7109a
Aaron Collegeman <aaron@collegeman.net>
2011-06-11 21:58:07 -0400

    Functional test for FileCache system

--- tests/FileCacheTests.php
+++ tests/FileCacheTests.php
@@ -5,11 +5,12 @@
 class FileCacheTests extends UnitTestCase {
   
   function testFileCache() {
-    $this->assertNotEqual('', coreylib('http://feeds.feedburner.com/github', '2 seconds')->get('title'));
+    $this->assertNotEqual('', coreylib('http://feeds.feedburner.com/github', '10 seconds')->get('title'));
     $this->assertTrue(file_exists(clFileCache::getLastPath()));
-    usleep(2500000);
-    $this->assertNotEqual('', coreylib('http://feeds.feedburner.com/github', -1)->get('title'));
-    $this->assertFalse(file_exists(clFileCache::getLastPath()));
+    // the only way to really test this is to run it more than once
+    // the second time you run it (within 10 seconds of the first), it should finish faster (no download)
+    // after the 10 second window, running it should raise a U_NOTICE_ERROR announcing that the
+    // cache has expired
   }
   
 }
